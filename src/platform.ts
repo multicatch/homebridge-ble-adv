cf20bd67911d37f83cf80fe96af48afc1a941a51
multicatch <cezaryre@gmail.com>
2026-05-13 21:56:05 +0200

    init

---
+++ src/platform.ts
@@ -0,0 +1,90 @@
+import type { API, Characteristic, DynamicPlatformPlugin, Logging, PlatformAccessory, PlatformConfig, Service } from 'homebridge';
+
+import { BLEAdvButtonAccessory } from './platformAccessory.js';
+import { PLATFORM_NAME, PLUGIN_NAME } from './settings.js'
+
+import noble, { Peripheral } from '@abandonware/noble';
+
+
+function scanAndNotify(log: Logger, buttons: BLEAdvButtonAccessory[]) {
+
+}
+
+/**
+ * HomebridgePlatform
+ * This class is the main constructor for your plugin, this is where you should
+ * parse the user config and discover/register accessories with Homebridge.
+ */
+export class BLEAdvHomebridgePlatform implements DynamicPlatformPlugin {
+  public readonly Service: typeof Service;
+  public readonly Characteristic: typeof Characteristic;
+
+  // this is used to track restored cached accessories
+  public readonly accessories: Map<string, PlatformAccessory> = new Map();
+  public readonly discoveredCacheUUIDs: string[] = [];
+
+  // This is only required when using Custom Services and Characteristics not support by HomeKit
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  public readonly CustomServices: any;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  public readonly CustomCharacteristics: any;
+
+  constructor(
+    public readonly log: Logging,
+    public readonly config: PlatformConfig,
+    public readonly api: API,
+  ) {
+    this.Service = api.hap.Service;
+    this.Characteristic = api.hap.Characteristic;
+
+    this.log.debug('Finished initializing platform:', this.config.name);
+
+    this.api.on('didFinishLaunching', () => {
+      log.debug('Executed didFinishLaunching callback');
+      const buttons = this.discoverDevices();
+      scanAndNotify(log, buttons);
+    });
+  }
+
+  /**
+   * This function is invoked when homebridge restores cached accessories from disk at startup.
+   * It should be used to set up event handlers for characteristics and update respective values.
+   */
+  configureAccessory(accessory: PlatformAccessory) {
+    this.log.info('Loading accessory from cache:', accessory.displayName);
+
+    // add the restored accessory to the accessories cache, so we can track if it has already been registered
+    this.accessories.set(accessory.UUID, accessory);
+  }
+
+  discoverDevices(): BLEAdvButtonAccessory[] {
+    const buttons = this.config.buttons;
+
+    const devices = [];
+    const configuredButtons: BLEAdvButtonAccessory[] = [];
+
+    for (const aButton of buttons) {
+      const uuid = this.api.hap.uuid.generate(aButton.deviceName);
+
+      // see if an accessory with the same uuid has already been registered and restored from
+      // the cached devices we stored in the `configureAccessory` method above
+      const existingAccessory = this.accessories.get(uuid);
+
+      if (existingAccessory) {
+        // the accessory already exists
+        this.log.info('Restoring existing accessory from cache:', existingAccessory.displayName);
+        this.api.unregisterPlatformAccessories(PLUGIN_NAME, PLATFORM_NAME, [existingAccessory]);
+      } else {
+        this.discoveredCacheUUIDs.push(uuid);
+      }
+
+      this.log.info('Adding new button: %s', aButton.name);
+      const accessory = new this.api.platformAccessory(aButton.name, uuid);
+      configuredButtons.push(new BLEAdvButtonAccessory(this.Characteristic, this.Service, accessory, aButton));
+      devices.push(accessory);
+    }
+
+    this.api.registerPlatformAccessories(PLUGIN_NAME, PLATFORM_NAME, devices);
+    return configuredButtons;
+  }
+}
---
+++ src/platformAccessory.ts
@@ -0,0 +1,38 @@
+import { Characteristic as ICharacteristic, PlatformAccessory, Service } from 'homebridge';
+
+export interface ButtonConfig {
+  name: string;
+  deviceName: string,
+  buttonPressAdvPattern: string,
+};
+
+export class BLEAdvButtonAccessory {
+  private service: Service;
+
+  constructor(
+    private readonly characteristic: typeof ICharacteristic,
+    serviceType: typeof Service,
+    private readonly accessory: PlatformAccessory,
+    private readonly config: ButtonConfig,
+  ) {
+    this.service = this.accessory.addService(serviceType.StatelessProgrammableSwitch)!;
+
+    this.service.setCharacteristic(characteristic.Name, config.name);
+    this.service.setCharacteristic(characteristic.ConfiguredName, config.name);
+
+    this.service.addCharacteristic(characteristic.ProgrammableSwitchEvent);
+
+    this.accessory.getService(serviceType.AccessoryInformation)!
+      .setCharacteristic(characteristic.Manufacturer, 'multicatch')
+      .setCharacteristic(characteristic.Model, 'Dummy Button')
+      .setCharacteristic(characteristic.FirmwareRevision, '1.0.0');
+  }
+
+  triggerEvent(event: number) {
+    this.service.updateCharacteristic(this.characteristic.ProgrammableSwitchEvent, event);
+  }
+
+  getAdvPattern(): RegExp {
+    return new RegExp(this.config.buttonPressAdvPattern);
+  }
+}
